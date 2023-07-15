6029b4ee1e129ed1d185b2c64f96839e4d01af10
CrazyBoy <65382951+CrazyBoy621@users.noreply.github.com>
2023-07-15 21:30:01 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,69 @@
+const allSections = document.querySelectorAll(".section");
+const allContents = document.querySelectorAll(".content");
+const allAnchorTags = document.querySelectorAll("a");
+
+// Store all live intersecting element id
+let intersectingIdList = [];
+
+// Store sequence of all sections ids
+let allSequenceIds = [];
+
+allSections.forEach((sectionElement) => {
+    allSequenceIds.push(sectionElement.id);
+});
+
+const observeElement = (element) => {
+    const obsCallback = function (entries, observer) {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                intersectingIdList.push(entry.target.id);
+            } else {
+                intersectingIdList = intersectingIdList.filter((id) => {
+                    if (id !== entry.target.id) return id;
+                });
+            }
+
+            let isFirstElementKnown = true;
+            let index = 0;
+            while (isFirstElementKnown && index < allSequenceIds.length) {
+                if (intersectingIdList.includes(allSequenceIds[index])) {
+                    setActiveContent(allSequenceIds[index]);
+                    isFirstElementKnown = false;
+                }
+                index++;
+            }
+        });
+    };
+
+    const obsOptions = {
+        root: null,
+        threshold: 0.1,
+    };
+
+    const observer = new IntersectionObserver(obsCallback, obsOptions);
+    observer.observe(element);
+};
+
+allSections.forEach((section) => {
+    observeElement(section);
+});
+
+function setActiveContent(id) {
+    allContents.forEach((content) => {
+        if (id === content.children[0].href.split("#")[1]) {
+            content.classList.add("active");
+        } else {
+            content.classList.remove("active");
+        }
+    });
+}
+
+allAnchorTags.forEach((element) => {
+    element.addEventListener("click", (e) => {
+        e.preventDefault();
+        const hash = element.href.split("#")[1];
+        document
+            .querySelector("#" + hash)
+            .scrollIntoView({ behavior: "smooth" });
+    });
+});
